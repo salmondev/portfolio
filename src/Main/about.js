6081d3c528c63be37f7446aa800afec095ddb396
Achiaga <alfonso.achiaga@gmail.com>
2020-09-17 12:38:39 +0200

    finsih about

--- src/Main/about.js
+++ src/Main/about.js
@@ -9,7 +9,7 @@ const Container = styled(motion.section)`
 	padding-right: 170px;
 	margin: 0px auto;
 	max-width: 1600px;
-	min-height: 90vh;
+	min-height: 100vh;
 	color: white;
 	overflow: hidden;
 	font-family: 'Asap', sans-serif;
@@ -86,6 +86,7 @@ const Text = styled.p`
 	margin: 0;
 	padding-bottom: ${(props) => (props.space ? '1em' : '0')};
 	font-size: 20px;
+	text-align: justify;
 	@media only screen and (max-width: 450px) {
 		padding: 0em;
 		margin: 0em;
@@ -107,30 +108,38 @@ const About = ({ aboutRef }) => {
 			<Body>
 				<TextContainer>
 					<Text>
-						I'm currently looking for new opportunities. I'm always open to new projects, ideas, job offers or
-						new conections. I love building new stuff so just send me a message and I will answer you 100%.
-					</Text>
-					<br />
-					<br />
-					<Text space={true}>
-						I have a profound passion for programming and new technology. I love to leanrn new things and
-						build stuff. I studied mechanical engineer although I always like computers, so leraning
-						programming started as a hobby, until I decided to make the big step and change my whole carrer
-						life into programming.
-					</Text>
-					<Text>
-						Instead of dropping of university to pursuit my dream of being a programmer, I decided to
-						finsihing my mechanical engineer degree and while learning and working as a programmer.
+						I'm currently looking for new opportunities. I'm always open to new projects, ideas, job
+						opportunities and new connections. I love building new stuff so just send me a message and I will
+						answer you 100%.
 					</Text>
 					<br />
 					<br />
 					<Text>
-						I spend all my free time (and more than my free time 😉) into programming. It is one of the best
-						decission I have ever made and so happy of having taken it.
+						I have a profound passion for programming and new technology. I love to learn new things and build
+						stuff. I studied mechanical engineer although I always like computers, learning programming
+						started as a hobby, until I decided to make the big step and change my whole career into
+						programming.
 					</Text>
+					<br />
 					<Text>
-						I am a happy person who enjoys work. So my dream company would be one with a really good
-						enviroment with my coworkers.
+						I don't like to quit. Once I start something I finish it, so instead of dropping out of college to
+						pursue my dream of being a programmer, I decided to finish my mechanical engineer degree while
+						learning and working as a programmer.
+					</Text>
+					<br />
+					<br />
+					<Text>
+						My preferable area is frontend using Reactjs, I love how React makes it so easy to improve the
+						user's experience. As an engineer, I like UX and the balance between UI and UX. I love to build
+						nice looking easy to use responsive web sites. I don't mind getting my hands on other areas if
+						needed like backend but my area of expertise is frontend.
+					</Text>
+					<br />
+					<br />
+					<Text>
+						Switching carrers is the best decission I have ever made and I am so happy with it. I am a happy
+						person who enjoys work. So my dream company would be one with a really good enviroment with my
+						coworkers.
 					</Text>
 				</TextContainer>
 			</Body>
